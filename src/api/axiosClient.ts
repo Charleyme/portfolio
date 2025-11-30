758cab75c161e08fe68e678afdaa68df6f60284f
Charleyme <charlesjiwueze826@gmail.com>
2025-11-30 13:36:08 +0100

    production

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,17 +1,20 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api",
 });
 
 // 🔥 Attach token automatically for every request
 axiosClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("adminToken");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
 export default axiosClient;
+
 
